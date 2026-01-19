6d2555579e89a758b9f98a926f3a6cb11bf2d39a
Scott3-0 <bremmerscottw@gmail.com>
2026-01-18 20:23:03 -0800

    break time

--- ProgAss1/ProgAss1-main.c
+++ ProgAss1/ProgAss1-main.c
@@ -1,19 +1,73 @@
 #include <stdio.h>
 #include <math.h>
 
+ 
 
-int getNumInRange(int min, int max){ //keep sking user for value in between min and max 
+int getWholeNumInRange(int min, int max){ //keep sking user for value in between min and max 
 	int n;
 	
 	do{
-		printf("Enter a num between %d and %d (inclusive):", min, max);
+		printf("\nEnter a whole num between %d and %d (inclusive):", min, max);
 		scanf("%d", &n);
 	}while(n < min || n > max);
 
 	return n;
 }
 
+float getNumInRange(int min, int max){ //keep sking user for value in between min and max
+        float n;
+
+        do{
+                printf("\nEnter a num between %d and %d (inclusive):", min, max);
+                scanf("%f", &n);
+        }while(n < min || n > max);
+
+        return n;
+}
+
+float getPosValue(void){
+	float n;
+	do{
+		printf("\nEnter a real number >= 0: ");
+		scanf("%f", &n);
+	}while(n < 0);
+
+	return n;
+}
+
+float calcSurfArea(float radius, float ha, float hb){
+	float a = sqrt((radius*radius) - (ha*ha));
+	float b = sqrt((radius*radius) - (hb*hb));
+	float h = ha - hb;
+
+}
+
+float calcVol(float radius, float ha, float hb){
+
+}
 
 int main(void){
-	int sphereSegments = getNumInRange(2, 10);
+	int sphereSegs = getWholeNumInRange(2, 10);
+	float ha[sphereSegs], hb[sphereSegs];
+	float radius;
+	float surfaceArea[sphereSegs], vol[sphereSegs];
+
+
+	radius = getPosValue();
+	
+	for(int i = 0; i < sphereSegs; i++){
+		printf("\nenter smaller h");
+		hb[i] = getNumInRange(0, radius);
+		printf("\nenter larger h");
+		ha[i] = getNumInRange(hb[i], radius);
+		
+		surfaceArea[i] = calcSurfArea(radius, ha[i], hb[i]);
+		vol[i] = calcVol(radius, ha[i], hb[i]);
+
+		printf("\n=============================");
+		printf("\nRadius: %f \n %f < height < %f", radius, hb[i], ha[i]);
+		printf("\n=============================");		
+	
+	
+	}
 }
